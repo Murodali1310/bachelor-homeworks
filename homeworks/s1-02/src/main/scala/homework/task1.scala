43083a26b347c23fd403c576aa784df475389c7f
Murodali1310 <144055219+Murodali1310@users.noreply.github.com>
2024-03-03 20:47:54 +0300

    add_task1

--- homeworks/s1-02/src/main/scala/homework/task1.scala
+++ homeworks/s1-02/src/main/scala/homework/task1.scala
@@ -1,3 +1,5 @@
+// task1.scala
+
 package homework
 
 import utils.Homeworks._
@@ -6,4 +8,29 @@ object task1 {
 
   trait Building
 
+  object UserPhone {
+    def apply(phone: String): Option[String] = {
+      val regex = raw"\+79\d{9}".r
+      if (regex.matches(phone)) Some(phone) else None
+    }
+  }
+
+  def main(args: Array[String]): Unit = {
+    val validRussianPhone = "+79123456789"
+    val invalidRussianPhone = "+78123456789"
+    val invalidPhone = "1234567890"
+
+    val validRussianPhoneResult = UserPhone(validRussianPhone)
+    val invalidRussianPhoneResult = UserPhone(invalidRussianPhone)
+    val invalidPhoneResult = UserPhone(invalidPhone)
+
+    println(s"Valid Russian phone: $validRussianPhone -> $validRussianPhoneResult")
+    println(s"Invalid Russian phone: $invalidRussianPhone -> $invalidRussianPhoneResult")
+    println(s"Invalid phone: $invalidPhone -> $invalidPhoneResult")
+
+    // Проверка на тестовые случаи
+    assert(UserPhone("+79123456789") == Some("+79123456789"))
+    assert(UserPhone("+78123456789") == None)
+    assert(UserPhone("1234567890") == None)
+  }
 }
